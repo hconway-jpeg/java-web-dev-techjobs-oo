57917ca5dcad2f003a3c226494d440045ed3f973
unknown <hconway@luc.edu>
2021-02-04 18:31:22 -0600

    completed through updating CoreCompetency class

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -13,11 +13,70 @@ public class Job {
 
     // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
-    //  the 'id' field.
+    //  the 'id' field. **DONE**
+
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String aName, Employer anEmployer, Location aLocation, PositionType aPositionType, CoreCompetency aCoreCompetency) {
+        this();
+        this.name = aName;
+        this.employer = anEmployer;
+        this.location = aLocation;
+        this.positionType = aPositionType;
+        this.coreCompetency = aCoreCompetency;
+    }
 
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
-    //  and id.
+    //  and id. **DONE**
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String aName) {
+        this.name = aName;
+    }
+
+
+    public Employer getEmployer() {
+        return employer;
+    }
+    public void setEmployer(Employer anEmployer) {
+        this.employer = anEmployer;
+    }
+
+
+    public Location getLocation() {
+        return location;
+    }
+    public void setLocation(Location aLocation) {
+        this.location = aLocation;
+    }
+
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+    public void setPositionType(PositionType aPositionType) {
+        this.positionType = aPositionType;
+    }
+
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+    public void setCoreCompetency(CoreCompetency aCoreCompetency) {
+        this.coreCompetency = aCoreCompetency;
+    }
 }
