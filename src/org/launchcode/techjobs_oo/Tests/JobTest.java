d6c25a48b64aac26e7f652c367ad9179d2f37370
unknown <hconway@luc.edu>
2021-02-06 18:40:35 -0600

    completed through step 4; unit tests passing, but need checking

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -10,12 +10,16 @@ public class JobTest {
     Job testJob1;
     Job testJob2;
     Job fancyJob;
+    Job unequalJob1;
+    Job unequalJob2;
 
     @Before
     public void createJobObject() {
         testJob1 = new Job();
         testJob2 = new Job();
         fancyJob = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        unequalJob1 = new Job("Job Title", new Employer ("LaunchCode"), new Location("Mushroom Forest"), new PositionType("Resident Genius"), new CoreCompetency( "Mad skills"));
+        unequalJob2 = new Job("Job Title", new Employer ("LaunchCode"), new Location("Mushroom Forest"), new PositionType("Resident Genius"), new CoreCompetency( "Mad skills"));
     }
 
 //the empty constructor is this()
@@ -26,7 +30,7 @@ public class JobTest {
 
     @Test
     public void testJobConstructorSetsAllFields() {
-        //assertTrue(fancyJob.getId() instanceof int);
+        //assertTrue(fancyJob.getId() instanceof int); **HELP**
         assertTrue(fancyJob.getName() instanceof String);
         assertTrue(fancyJob.getEmployer() instanceof Employer);
         assertTrue(fancyJob.getLocation() instanceof Location);
@@ -34,8 +38,13 @@ public class JobTest {
         assertTrue(fancyJob.getCoreCompetency() instanceof CoreCompetency);
     }
 
+    @Test
+    public void testJobsForEquality() {
+        assertFalse(unequalJob1 == unequalJob2);
+    }
+
 //    @Test
-//    public void testJobsForEquality() {
+//    public void testToStringMethod() {
 //        //
 //    }
 
